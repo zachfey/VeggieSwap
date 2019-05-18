7bcda053830ae5952e4ae160fbf97749c9dfffc2
Zach <zachary.fey@gmail.com>
2019-05-18 13:56:37 -0400

    completed basic pull and post requests; need to implement with front-end

--- public/js/index.js
+++ public/js/index.js
@@ -6,23 +6,23 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
+      url: "api/deals",
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -31,9 +31,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -61,20 +61,36 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  // Create a new examplee
+  // TODO: un-hard code this object
+  var newObj = {
+    UserId: 1,
+    offered: 'Cherries',
+    offeredQTY: 50,
+    asked: 'Oranges',
+    askedQTY: 5,
+    status: 'pending'
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+  // To create new user:
+  // var newObj = {
+  //   username: "zachary",
+  //   password: 'password',
+  //   email: 'email'
+  // }
 
-  API.saveExample(example).then(function() {
+  // if (!(example.text && example.description)) {
+  //   alert("You must enter an example text and description!");
+  //   return;
+  // }
+
+
+
+
+  API.saveExample(newObj).then(function () {
     refreshExamples();
   });
 
@@ -84,12 +100,12 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,23 +26,23 @@ module.exports = function (app) {
   }
 
   app.get("/", function (req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    res.render("index", {deals: testObject})
-      // });
-    });
-  // });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    db.Deal.findAll({}).then(function (results) {
+      // console.log(results)
+      res.render("index", { deals: results })
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+  //   // Load example page and pass in an example by id
+  //   app.get("/example/:id", function (req, res) {
+  //     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+  //       res.render("example", {
+  //         example: dbExample
+  //       });
+  //     });
+  //   });
+
+  //   // Render 404 page for any unmatched routes
+  //   app.get("*", function (req, res) {
+  //     res.render("404");
+  //   });
 };
