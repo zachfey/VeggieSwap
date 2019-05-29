10751a57181c957b853cd3e8db67b637f7531b7f
ascinitially <Blaise@Blaises-MacBook-Pro.local>
2019-05-29 19:15:54 -0400

    crunch time

--- models/deals.js
+++ models/deals.js
@@ -12,7 +12,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    askedUnits: {
+    offererUnits: {
       type: DataTypes.STRING,
       allowNull: false
     },
